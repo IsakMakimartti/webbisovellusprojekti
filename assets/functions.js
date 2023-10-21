4c927d5aa1381e1b2b9e4da49becea5b3794cdb4
IsakMakimartti <t2mais00@students.oamk.fi>
2023-10-21 20:20:46 +0300

    patch 0.23
    updates

--- assets/functions.js
+++ assets/functions.js
@@ -1,7 +1,7 @@
 function whiteMode() {
-    var element = document.body;
-    element.classList.toggle("white-mode");
-  } 
+  var element = document.body;
+  element.classList.toggle("white-mode");
+}
 
 function alertBox() {
   alert("Submitted! Thanks you for answering the survey!");
@@ -9,4 +9,21 @@ function alertBox() {
 
 function playAudio(url) {
   new Audio(url).play();
+}
+
+function copyTextFromLi(liElement) {
+  // Get the text from the li element
+  var textToCopy = liElement.textContent;
+
+  // Remove the "Email: " or "Phone: " text from the beginning of the string
+  textToCopy = textToCopy.replace("Email: ", "").replace("Phone: ", "");
+  
+  var tempElement = document.createElement('textarea');
+  tempElement.value = textToCopy;
+  document.body.appendChild(tempElement);
+  tempElement.select();
+  tempElement.setSelectionRange(0, 99999); // For mobile devices
+  document.execCommand('copy');
+  document.body.removeChild(tempElement);
+  alert('Copied the text: ' + textToCopy);
 }

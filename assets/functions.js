fdbfff9d8078d71d76b361b407fbb7fadab80274
IsakMakimartti <t2mais00@students.oamk.fi>
2023-10-21 18:40:13 +0300

    patch 0.21
    some upgrades

--- assets/functions.js
+++ assets/functions.js
@@ -1,4 +1,12 @@
-function myFunction() {
+function whiteMode() {
     var element = document.body;
     element.classList.toggle("white-mode");
   } 
+
+function alertBox() {
+  alert("Submitted! Thanks you for answering the survey!");
+}
+
+function playAudio(url) {
+  new Audio(url).play();
+}
